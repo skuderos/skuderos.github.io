9de632cb73a1abbe607629ab9f7adddf711ad948
skuderos <jordoe@live.nl>
2022-08-16 21:37:37 +0200

    Update userlist.js

--- userlist.js
+++ userlist.js
@@ -16,12 +16,43 @@ const userdata = [];
 
 userdata.push(username_guest);
 userdata.push(username_admin);
-let usernamegiven = "guest";
+
 
 //function that sets the current search key and retrieves the object
 function userreadtest(){
-let selectedobj = userdata.find(o => o.username === usernamegiven);
 
-console.log(selectedobj);
+  //get the current hash and username that is filled in
+let usernamegiven = document.querySelector('[data-id="userlogin"]').value;
+let hashgiven = sha256(document.querySelector('[data-id="pwlogin"]').value);
+console.log(usernamegiven);
+console.log(hashgiven);
+
+try{
+
+  //get the expected hash for the given username
+let selectedobj = userdata.find(o => o.username === usernamegiven);
+let expectedhash=selectedobj.hash;
+console.log(expectedhash);
+console.log(hashgiven);
+
+//check whether the hash matches the expected value. If it does, set the correct switches. If not, set switches for authentication fail OR using the error catch for the situation where the user isnt found in the database
+if(expectedhash == hashgiven){
+  setValueForVariable("postauthenticationtext", 'TRUE');
+  setValueForVariable("authenticated", 'TRUE');
+  setValueForVariable("authentfailed", 'FALSE');
+  setValueForVariable("usernotfound", 'FALSE');
+  console.log("it's equal!");
+} else{
+  console.log("it's not equal");
+    setValueForVariable("authentfailed", 'TRUE');
+    setValueForVariable("usernotfound", 'FALSE');
+}
+
+}
+
+catch(err){
+console.log("username not found in database");
+  setValueForVariable("usernotfound", 'TRUE');
+}
 
 }
