89c93a3fe55321b4802054d6ac33e5ca96d44f5f
skuderos <jordoe@live.nl>
2022-08-16 15:48:54 +0200

    Create userlist.js

---
+++ userlist.js
@@ -0,0 +1,2 @@
+var user1 = '{"username":"admin", "hash":"8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918"}';
+var user2 = '{"username":"guest", "hash":"84983c60f7daadc1cb8698621f802c0d9f9a3c3c295c810748fb048115c186ec"}';
