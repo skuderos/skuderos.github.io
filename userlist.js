854b648953141467a74d4f2f5c2925d29542f2fb
skuderos <jordoe@live.nl>
2022-08-16 19:14:13 +0200

    Update userlist.js

--- userlist.js
+++ userlist.js
@@ -1,17 +1,27 @@
-var user1 = {};
-user1.name = "admin";
-user1.hash = "8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918";
+//list of user objects
+const username_admin = {
+username:"admin",
+hash:"8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918",
+accesslevel:"1",
+};
 
-var user2 = {};
-user2.name= "guest";
-user2.hash= "84983c60f7daadc1cb8698621f802c0d9f9a3c3c295c810748fb048115c186ec";
+const username_guest = {
+username:"guest",
+hash:"84983c60f7daadc1cb8698621f802c0d9f9a3c3c295c810748fb048115c186ec",
+accesslevel:"2",
+};
 
-let userscombined = [user1 ,user2];
+//creation of combined array and pushing of each object into it
+const userdata = [];
 
+userdata.push(username_guest);
+userdata.push(username_admin);
+
+
+//function that sets the current search key and retrieves the object
 function userreadtest(){
-var usernamegiven = "admin"
-console.log(user1)
-console.log(usernamegiven);
-var foundhash = JSON.parse(userscombined.usernamegiven)
-console.log(foundhash)
+let selectedobj = userdata.find(o => o.username === usernamegiven);
+
+console.log(selectedobj);
+
 }
