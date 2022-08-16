3f08ed972e2265ddb335ad5254e092a6541d7a3a
skuderos <jordoe@live.nl>
2022-08-16 15:56:42 +0200

    Update userlist.js

--- userlist.js
+++ userlist.js
@@ -1,2 +1,11 @@
-var user1 = '{"username":"admin", "hash":"8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918"}';
-var user2 = '{"username":"guest", "hash":"84983c60f7daadc1cb8698621f802c0d9f9a3c3c295c810748fb048115c186ec"}';
+var user1 = '{"admin":"8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918"}';
+var user2 = '{"guest":"84983c60f7daadc1cb8698621f802c0d9f9a3c3c295c810748fb048115c186ec"}';
+
+let userscombined = [user1 ,user2];
+
+function userreadtest(){
+var usernamegiven = "admin"
+console.log(userreadtest);
+var foundhash = JSON.parse(userscombined.usernamegiven)
+console.log(foundhash)  
+}
