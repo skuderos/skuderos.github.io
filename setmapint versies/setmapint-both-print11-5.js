4b6f32c93d2386f349b911a2c7eca9bb6162f7e5
skuderos <jordoe@live.nl>
2022-10-16 14:25:06 +0200

    hh

--- setmapint versies/setmapint-both-print11-5.js
+++ setmapint versies/setmapint-both-print11-5.js
@@ -172,7 +172,7 @@ var gpsplaneicon = L.icon({
 
     iconSize:     [50, 50], // size of the icon
     //shadowSize:   [50, 64], // size of the shadow
-    iconAnchor:   [25, 35], // point of the icon which will correspond to marker's location
+    iconAnchor:   [45, 35], // point of the icon which will correspond to marker's location
     //shadowAnchor: [4, 62],  // the same for the shadow
     popupAnchor:  [0, 0] // point from which the popup should open relative to the iconAnchor
 });
